244093190ae0faed822b6c4ce44960372697f631
Justyna Szadowiak <justyna.elzbieta.szadowiak@gmail.com>
2022-07-29 23:24:19 +0200

    Typography has been added, login component has been craeted to app

---
+++ src/components/typography/typography.js
@@ -0,0 +1,13 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function Typography() {
+    <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+}
+
+export default Typography;
